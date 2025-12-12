9fe9c878184e0e1cce052ca847efcbfd24654d9e
Ed Karrels <ed.karrels@gmail.com>
2025-12-11 16:31:51 -0800

    days 10 and 11 done, just need to update README

---
+++ day10.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+
+"""
+Advent of Code 2025, Day 10: Factory
+
+
+
+Ed Karrels, ed.karrels@gmail.com, December 2025
+"""
+
+# common standard libraries
+import sys, os, re, itertools, math, collections, itertools
+import numpy as np
+
+# linear programming library
+import pulp
+
+# change to the directory of the script so relative references work
+from pathlib import Path
+script_dir = Path(__file__).resolve().parent
+os.chdir(str(script_dir))
+from advent import *
+
+
+class Machine:
+    def __init__(self, lights, buttons, button_bits, jolts):
+        self.lights = lights
+
+        # list of tuples, just like the input
+        self.buttons = buttons
+
+        # list of integers with the given bits set
+        # i.e. (0,1,2,3) -> 15
+        self.button_bits = button_bits
+        
+        self.jolts = jolts
+
+    line_re = re.compile(r'\[([#.]*)\] (.*) {(.*)}')
+        
+    @staticmethod
+    def parse(line):
+        """
+        Parse line like this:
+          [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+        into a Machine object.
+        The first part [.##.] is a bit mask: 0110
+        The second part '(3) (1,3), ...' is a bit mask for the part1
+          problem, and a list of indices for part2.
+        The third part {3,5,4,7} is a list of joltage values for part 3.
+        """
+        match = Machine.line_re.match(line)
+        assert match
+        light_str, buttons_str, jolts_str = match.groups()
+
+        bit = 1
+        lights = 0
+        for c in light_str:
+            if c == '#':
+                lights += bit
+            bit *= 2
+
+        def button_positions_to_int(positions):
+            return sum([2**x for x in positions])
+            
+        button_ints = [tuple([int(v) for v in b[1:-1].split(',')])
+                       for b in buttons_str.split(' ')]
+        button_bits = [button_positions_to_int(b) for b in button_ints]
+
+        jolts = [int(x) for x in jolts_str.split(',')]
+
+        return Machine(lights, button_ints, button_bits, jolts)
+
+
+def light_button_count_bfs(machine):
+    # each tuple on the queue:
+    # (current_lights, list of indices of buttons pressed)
+    q = collections.deque()
+    q.append((0, []))
+
+    while len(q) > 0:
+        current_lights, buttons_pressed = q.popleft()
+        if current_lights == machine.lights:
+            return buttons_pressed
+
+        next_bi = 0 if len(buttons_pressed)==0 else buttons_pressed[-1]
+        for bi in range(next_bi, len(machine.button_bits)):
+            next_lights = current_lights ^ machine.button_bits[bi]
+            pressed = buttons_pressed + [bi]
+            q.append((next_lights, pressed))
+
+    return None
+
+
+def part1(machines):
+    button_press_total = 0
+    for i, m in enumerate(machines):
+        # print(f'machine {i}')
+        # print(f'  {m.lights}')
+        # print(f'  {m.buttons!r}')
+        # print(f'  {m.button_bits!r}')
+        # print(f'  {m.jolts!r}')
+        pressed_list = light_button_count_bfs(m)
+        button_press_total += len(pressed_list)
+        # print(f'  soln = {pressed_list!r}')
+
+    # 434 is wrong (right for someone else)
+    print(button_press_total)
+
+
+def joltage_presses(machine):
+    """
+    1*(3)
+    3*(1,3)
+    0*(2)
+    3*(2,3)
+    1*(0,2)
+    2*(0,1)
+
+    0: 3
+    1: 5
+    2: 4
+    3: 7
+
+    This problem is finding a vector x such that A x = b in an
+    underconstrained system, looking for a minimum sum of the elements
+    in vector x.
+    Also:
+      - the input matrix is all 0's and 1's.
+      
+    
+    [0 0 0 0 1 1  * [1 = [3
+     0 1 0 0 0 1     3    5
+     0 0 1 1 1 0     0    4
+     1 1 0 1 0 0]    3    7]
+                     1    
+                     2]
+
+    Matrix
+      each column is the bits in a button
+      one column per button
+    goal = jolts vector
+    solution vector = number of times each buttons should be pressed
+
+    1 * 1000
+   +3 * 1010
+   +0 *  100
+   +3 * 1100
+   +1 *  101
+   +2 *   11
+
+    FrobeniusSolve[{1000, 1010, 100, 1100, 101, 11}, 7453]
+      finds the solution, along with 4000 others
+    
+    """
+
+    height = len(machine.jolts)
+    width = len(machine.buttons)
+
+    A = np.zeros((height, width))
+    for c in range(width):
+        button = machine.buttons[c]
+        for r in button:
+            A[r][c] = 1
+
+    goal = np.array(machine.jolts)
+
+    prob = pulp.LpProblem("find_x", pulp.LpStatusOptimal)
+    x_vars = [pulp.LpVariable(f"x{i}", lowBound=0, cat="Integer")
+              for i in range(width)]
+
+    # equality constraints
+    for i in range(height):
+        prob += pulp.lpSum(A[i,j]*x_vars[j] for j in range(width)) == goal[i]
+
+    # find smallest solution
+    prob += pulp.lpSum(x_vars)
+
+    timer = time.perf_counter_ns()
+    # prob.solve()    # default CBC solver
+    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+    timer = time.perf_counter_ns() - timer
+
+    if pulp.LpStatus[prob.status] != "Optimal":
+        print("no integer solution")
+        return None
+    
+    soln_float = [v.value() for v in x_vars]
+    soln = [round(v) for v in soln_float]
+    
+    # soln_vector = np.array(soln)
+    # print(f'A x = {A @ soln_vector}')
+    # check_float = [v for v in A @ soln_vector]
+    # check = [round(v) for v in check_float]
+    # if check != machine.jolts:
+    #     print(f'ERROR, goal={machine.jolts}, result={check} or {check_float}')
+    # print(repr(check))
+
+    return sum(soln)
+    
+        
+def part2(machines):
+    total_presses = 0
+    for i, machine in enumerate(machines):
+        # print(f'machine {i}')
+        total_presses += joltage_presses(machine)
+    print(total_presses)
+
+if __name__ == '__main__':
+    # read input as a list of strings
+    input = read_problem_input()
+    machines = [Machine.parse(line) for line in input]
+  
+    t0 = time.perf_counter_ns()
+    part1(machines)
+    t1 = time.perf_counter_ns()
+    part2(machines)
+    t2 = time.perf_counter_ns()
+    print(f'part1 {(t1-t0)/1e6:.2f} millis')
+    print(f'part2 {(t2-t1)/1e6:.2f} millis')
--- day11.py
+++ day11.py
@@ -3,10 +3,7 @@
 """
 Advent of Code 2025, Day 11: Reactor
 
-
-Start at "you", end at "out"
-Is a DAG (no cycles)
-
+Counting paths through a graph
 
 Ed Karrels, ed.karrels@gmail.com, December 2025
 """
@@ -22,39 +19,89 @@ from advent import *
 
 
 class Node:
-    def __init__(self, line):
-        self.name = line[:3]
+    def __init__(self, name):
+        self.name = name
 
         # outgoing nodes
-        # initially store the names of each outgoing node, then when
-        # all nodes have been read, replaced those with node objects.
-        self.outs = line[5:].split()
+        self.outs = []
 
         # incoming nodes
-        # initially a set of names, then changed to a list of objects
-        self.ins = set()
+        self.ins = []
 
-    def __str__(self):
+        # used in path counting, this is the number of paths leading
+        # to this node from some starting point
+        self.n_paths = 0
+
+    @staticmethod
+    def create_from_input_line(line):
+        node = Node(line[:3])
+        node.outs = line[5:].split()
+        return node
+
+    def __repr__(self):
         outs = ' '.join([o.name for o in self.outs])
         ins = ' '.join([o.name for o in self.ins])
         return f'Node({self.name} outs=({outs}) ins=({ins}))'
 
+    # without these, it just use the pointer to the object, which should
+    # be fine
+    
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def has_all_n_paths(self):
+        """
+        Returns True if all the input nodes for this node have n_paths set.
+        Without their n_paths set I can't compute my n_paths value.
+        """
+        for incoming in self.ins:
+            if incoming.n_paths == None:
+                return False
+        return True
+
+    def set_n_paths(self):
+        """
+        Sets my n_paths to be the sum of all my incoming nodes' n_paths.
+        Return True if successful, False if any of them are net set.
+        """
+        if self.n_paths != None:
+            return self.n_paths
+        
+        n_paths = 0
+        for incoming in self.ins:
+            if incoming.n_paths == None:
+                return False
+            n_paths += incoming.n_paths
+        self.n_paths = n_paths
+        
+        return True
+
 
 def parse_input(input):
-    node_list = [Node(line) for line in input]
-    node_map = {n.name: n for n in node_list}
+    # name: Node
+    node_map = {}
 
+    # first just create an object for each name
+    for line in input:
+        name = line[:3]
+        node = Node(name)
+        node_map[name] = node
+        
     # only listed as a destination, never a source
-    node_map['out'] = Node('out: ')
-    
-    for src in node_list:
-        src.outs = [node_map[dest_name] for dest_name in src.outs]
+    node_map['out'] = Node('out')
 
+    for line in input:
+        src = node_map[line[:3]]
+
+        # build src's 'outs' list
+        src.outs = [node_map[d] for d in line[5:].split()]
+
+        # add an entry to the destination node's 'ins' list
         for dest in src.outs:
-            dest.ins.add(src.name)
-
-    for node in node_list:
-        node.ins = [node_map[name] for name in node.ins]
+            dest.ins.append(src)
             
     return node_map
 
@@ -72,23 +119,22 @@ def output_graphviz(node_map):
         for out in node.outs:
             print(f'{node.name} -> {out.name};')
     print('}')
-          
 
 
-def count_routes(node, dest, path_count = [0]):
+def count_routes_dfs(node, dest, path_count = [0]):
     if node == dest:
         path_count[0] += 1
         return path_count[0]
 
     for child in node.outs:
         # print(f'{node.name} -> {child.name}')
-        count_routes(child, dest, path_count)
+        count_routes_dfs(child, dest, path_count)
 
     return path_count[0]
 
 
 def part1(node_map):
-    print(count_routes(node_map['you'], node_map['out']))
+    print(count_routes_dfs(node_map['you'], node_map['out']))
 
 
 REPORT_FREQ = 10000000
@@ -109,7 +155,7 @@ def find_choke_points(src, dest, node_map):
     pass
 
 
-def find_part2(node, path_count, visited, depth=0):
+def part2_dft_too_slow(node, path_count, visited, depth=0):
     global next_report, REPORT_FREQ
     if node.name == 'out':
         # print('at out')
@@ -140,9 +186,179 @@ def find_part2(node, path_count, visited, depth=0):
     for child in node.outs:
         # prefix = ' ' * depth
         # print(f'{prefix}{node.name} -> {child.name}')
-        find_part2(child, path_count, visited, depth+1)
+        part2_dft_too_slow(child, path_count, visited, depth+1)
 
     visited.remove(node.name)
+
+
+def reset_path_counts(node_map):
+    for node in node_map.values():
+        node.n_paths = 0
+
+
+def find_descendent_subset(src):
+    """
+    Returns a set of all the nodes reachable when starting at src
+    by doing a breadth-first traverse of 'outs' lists.
+    """
+    visited = set()
+
+    visit_q = collections.deque()
+    visit_q.append(src)
+    while len(visit_q):
+        node = visit_q.popleft()
+        if node in visited:
+            continue
+        visited.add(node)
+
+        for child in node.outs:
+            visit_q.append(child)
+
+    return visited
+
+
+def find_ancestor_subset(dest):
+    """
+    Returns a set of all the nodes that can reach dest
+    by doing a breadth-first traverse of 'ins' lists.    
+    """
+    visited = set()
+
+    visit_q = collections.deque()
+    visit_q.append(dest)
+    while len(visit_q):
+        node = visit_q.popleft()
+        if node in visited:
+            continue
+        visited.add(node)
+
+        for child in node.ins:
+            visit_q.append(child)
+
+    return visited
+
+
+def create_subgraph(node_map, selected_set):
+    """
+    Returns a name->node map of just the nodes in the graph included
+    in selected_set. This also remove entries from each node's 'ins' and
+    'outs' lists that are not in selected_set.
+    """
+    sub_node_map = {
+        n.name: Node(n.name) for n in node_map.values() if n in selected_set
+    }
+
+    # convert a node from node_map into a node in sub_node_map
+    def to_sub(orig):
+        return sub_node_map[orig.name]
+
+    for node in sub_node_map.values():
+        orig = node_map[node.name]
+        node.outs = [to_sub(x) for x in orig.outs if x in selected_set]
+        node.ins = [to_sub(x) for x in orig.ins if x in selected_set]
+
+    return sub_node_map
+
+
+def create_reachable_subgraph(src, dest):
+    """
+    returns a node_map consisting of just the nodes reachable from src
+    new nodes are created, with 'ins' and 'outs' reflecting this subgraph.
+    """
+
+    print(f'creating reachable subgraph from {src.name}')
+    
+    # nodes visited
+    visited = {}
+
+    visit_q = collections.deque()
+    visit_q.append(src)
+
+    while len(visit_q):
+        node = visit_q.popleft()
+        if node.name not in visited:
+            visited[node.name] = node
+        for child in node.outs:
+            visit_q.append(child)
+
+    print(f'{len(visited)} nodes in subgraph')
+    for node in visited.values():
+        print(node)
+            
+    node_map = {}
+    for name in visited:
+        pass
+
+    return visited
+    
+
+
+def count_paths_dp(src, dest):
+    path_map = {}
+    # path_map = {src: 1}
+    
+    q = collections.deque()
+    q.append(src)
+
+    def is_all_incoming_computed(node):
+        for incoming in node.ins:
+            if incoming not in path_map:
+                return False
+        return True
+
+    while len(q):
+        node = q.popleft()
+        
+        if node == src:
+            path_count = 1
+        else:
+            path_count = sum([path_map[incoming] for incoming in node.ins])
+        path_map[node] = path_count
+        # print(f'{path_count} paths to {node.name}')
+
+        for out in node.outs:
+            if is_all_incoming_computed(out):
+                q.append(out)
+                    
+    return path_map[dest]
+
+
+def test_depths(node_map):
+    """
+    Check if every route from the root to a node is the same length.
+    Answer: yes, many are reachable via paths of different lengths.
+    """
+    if len(node_map) < 100:
+        root = node_map['you']
+    else:
+        root = node_map['svr']
+        
+    node_depths = {root: 0}
+    visited = set()
+    
+    q = collections.deque()
+    q.append(root)
+    n_checked = 0
+
+    while len(q):
+        node = q.popleft()
+        if node in visited:
+            continue
+        visited.add(node)
+        n_checked += 1
+        
+        depth = node_depths[node]
+
+        for child in node.outs:
+            if child in node_depths:
+                if node_depths[child] != depth + 1:
+                    print(f'Inconsistency: {child} is at depths '
+                          f'{node_depths[child]} and {depth+1}')
+            else:
+                node_depths[child] = depth + 1
+            q.append(child)
+
+    print(f'{n_checked} checked.')
     
     
 
@@ -161,11 +377,15 @@ def part2(input):
     out = node_map['out']
     # find_part2(node, path_count, visited)
 
-    routes_to_fft = count_routes(root, fft)
-    print(f'{routes_to_fft} routes from root to fft')
+    # routes_root_to_fft = count_paths_bfs(root, fft, node_map)
+    # print(f'{routes_root_to_fft} routes from root to fft')
 
-    routes_from_dac = count_routes(dac, out)
-    print(f'{routes_from_dac} routes from dac to out')
+    # routes_dac_to_count = count_paths_bfs(dac, out, node_map)
+    # print(f'{routes_dac_to_count} routes from dac to out')
+
+    # n = count_paths_bfs(node_map['you'], node_map['out'], node_map)
+
+    m = create_reachable_subgraph(node_map['you'])
 
 
 
@@ -181,6 +401,30 @@ if __name__ == '__main__':
     # print(node_map['you'])
     # print(node_map['svr'])
     
-    part1(node_map)
+    # part1(node_map)
+
+    # test_depths(node_map)
+
+    fft_in_set = find_ancestor_subset(node_map['fft'])
+    fft_out_set = find_descendent_subset(node_map['fft'])
+    dac_in_set = find_ancestor_subset(node_map['dac'])
+    dac_out_set = find_descendent_subset(node_map['dac'])
+
+    subgraph_fft_in = create_subgraph(node_map, fft_in_set)
+    paths_to_fft = count_paths_dp(subgraph_fft_in['svr'],
+                                  subgraph_fft_in['fft'])
+    print(paths_to_fft)
+    print()
+
+    subgraph_fft_to_dac = create_subgraph(node_map, fft_out_set & dac_in_set)
+    paths_fft_to_dac = count_paths_dp(subgraph_fft_to_dac['fft'],
+                                      subgraph_fft_to_dac['dac'])
+    print(paths_fft_to_dac)
+    print()
     
-    # part2(node_map)
+    subgraph_dac_out = create_subgraph(node_map, dac_out_set)
+    paths_dac_out = count_paths_dp(subgraph_dac_out['dac'],
+                                   subgraph_dac_out['out'])
+    print(paths_dac_out)
+    print()
+    print(paths_to_fft * paths_fft_to_dac * paths_dac_out)
